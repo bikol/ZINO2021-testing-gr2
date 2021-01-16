34efbe9640275e35e476ab55067426377abef7c7
s457946 <s457946@pcd2n.labs.wmi.amu.edu.pl>
2021-01-16 11:34:31 +0100

    Implementacja dodawania double

--- Arytmetyka/src/main/java/pl/edu/amu/wmi/bikol/dino/arytmetyka/Dodawanie.java
+++ Arytmetyka/src/main/java/pl/edu/amu/wmi/bikol/dino/arytmetyka/Dodawanie.java
@@ -5,6 +5,8 @@
  */
 package pl.edu.amu.wmi.bikol.dino.arytmetyka;
 
+import java.text.DecimalFormat;
+
 /**
  *
  * @author bikol
@@ -12,6 +14,13 @@ package pl.edu.amu.wmi.bikol.dino.arytmetyka;
 public class Dodawanie {
     
     public String dodaj(String a, String b){
+        
+    if (a.contains(".") && b.contains(".")) {
+        DecimalFormat df = new DecimalFormat("#.##"); 
+        
+        return Double.toString(Double.valueOf(df.format(Double.parseDouble(a) + Double.parseDouble(b))));
+    }
+        
     try {
         Integer.parseInt(a);
         Integer.parseInt(b);
