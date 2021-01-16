408b174722564d7a2b2266297c50a0515810813a
s457990 <s457990@pcd2w.labs.wmi.amu.edu.pl>
2021-01-16 11:08:45 +0100

    Poprawienie implementacji

--- Arytmetyka/src/main/java/pl/edu/amu/wmi/bikol/dino/arytmetyka/Dodawanie.java
+++ Arytmetyka/src/main/java/pl/edu/amu/wmi/bikol/dino/arytmetyka/Dodawanie.java
@@ -12,7 +12,14 @@ package pl.edu.amu.wmi.bikol.dino.arytmetyka;
 public class Dodawanie {
     
     public String dodaj(String a, String b){
+    try {
+        Integer.parseInt(a);
+        Integer.parseInt(b);
         return Integer.toString(Integer.parseInt(a) + Integer.parseInt(b));
     }
+    catch( Exception e ) {
+        return a+b;
+        }
     
+    }
 }
